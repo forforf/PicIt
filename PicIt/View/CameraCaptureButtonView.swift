f4221f9f440a6b238f91e5ae8f137f9004d53ce5
forforf <dmarti21@gmail.com>
2022-05-02 12:01:52 -0400

    Remove CountdownView
    We moved the countdown to the overlay instead over the button.

--- PicIt/View/CameraCaptureButtonView.swift
+++ PicIt/View/CameraCaptureButtonView.swift
@@ -2,6 +2,27 @@
 
 import SwiftUI
 
+extension CountdownState {
+    
+    func cameraButtonColor() -> Color {
+        switch self {
+        case .stopped:
+            return .gray
+        case .inProgress:
+            return .yellow
+        case .triggering:
+            return .green
+        case .complete:
+            return .mint
+        case .undefined:
+            return .black
+        case .ready:
+            return .white
+        }
+    }
+    
+}
+
 struct CameraCaptureButton: View {
     static let log = PicItSelfLog<CameraCaptureButton>.get()
     
@@ -15,8 +36,16 @@ struct CameraCaptureButton: View {
     //    ok for CameraCaptureModel([weak]? cameraAction, countdown?)
     
     var buttonView: some View {
-        HStack {
-            CountdownView(countdownTimer: countdownTime, countdownState: countdownState)
+        ZStack {
+            Circle()
+                .foregroundColor(countdownState.cameraButtonColor())
+                .frame(width: 80, height: 80, alignment: .center)
+                .overlay(
+                    Circle()
+                        .stroke(Color.black.opacity(0.8), lineWidth: 2)
+                        .frame(width: 65, height: 65, alignment: .center)
+                )
+
             mediaMode.indicatorView()
         }
     }
